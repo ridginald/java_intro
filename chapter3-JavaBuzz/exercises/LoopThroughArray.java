d14df1ca6fa07d3b2f13c44d5ad3f032ca61049f
Daniel Ridgway <ridginald@gmail.com>
2018-07-03 16:25:13 +0100

    completed chap3 LoopThroughArray

--- chapter3-JavaBuzz/exercises/LoopThroughArray.java
+++ chapter3-JavaBuzz/exercises/LoopThroughArray.java
@@ -1,3 +1,15 @@
+
+public class LoopThroughArray {
+
+  public static void main(String[] args) {
+    int[] arr = {1, 2, 3, 4, 5};
+    System.out.println(arr.toString());
+    for (int i = 0; i < arr.length; i++) {
+      System.out.println(arr[i]);
+    }
+  }
+}
+
 // can you remember how to set up your main function?
 
 // loop through an integer array and print out each element
