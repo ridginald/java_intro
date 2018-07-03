bb17c2350b84181de18352ca5fd0b30609fc30eb
Daniel Ridgway <ridginald@gmail.com>
2018-07-03 16:35:25 +0100

    completed chap3 JavaBuzz

--- chapter3-JavaBuzz/exercises/JavaBuzz.java
+++ chapter3-JavaBuzz/exercises/JavaBuzz.java
@@ -1,3 +1,22 @@
+public class JavaBuzz {
+  public static void main(String[] args) {
+    int num = Integer.parseInt(args[0]);
+    if (num % 15 == 0) {
+      System.out.println("FizzBuzz");
+    }
+    else if (num % 5 == 0) {
+      System.out.println("Buzz");
+    }
+    else if (num % 3 == 0) {
+      System.out.println("Fizz");
+    }
+    else {
+      System.out.println(num);
+    }
+  }
+}
+
+
 // can you remember how to set up your main function?
 
 // "Java" if divisible by 3
